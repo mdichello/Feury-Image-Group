eef1d44e9f7f17cc71fbe061a19437518c2f7ba9
rachid-el-kedmiri <1028029592@usmba.ac.ma>
2021-03-08 15:21:13 +0100

    [ADD] Added module to differentiate between customer and supplier

---
+++ is_customer_is_supplier/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+	'name': 'Customer and Supplier Option on partner sheet',
+	'version': '13.0.0.0',
+	'category': 'Extra Tools',
+	'summary': 'Make a difference between customer and supplier' ,
+	'website': 'businesssoftwaredevelopmentsolutions.com',
+	'description':	'',
+	'author': 'Business Software Development Solutions',
+	'depends': ['base','sale_management','purchase','contacts'],
+	'data': [
+		'views/res_partner_view.xml',
+	],
+	'demo': [],
+	'qweb': [],
+	'installable': True,
+	'auto_install': True,
+	'application': False,
+}
---
+++ is_customer_is_supplier/models/res_partner.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models, _
+
+
+class Res_Partner(models.Model):
+	_inherit = 'res.partner'
+
+	is_customer = fields.Boolean(
+		string="Is a Customer",
+		default=True
+	)
+
+	is_vendor = fields.Boolean(
+		string="Is a Vendor",
+		default=True
+	)
