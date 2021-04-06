50aaac9090ed346e76b73a07ae76c5acd088b532
rachid-el-kedmiri <1028029592@usmba.ac.ma>
2021-04-06 17:35:57 +0100

    [IMP] Cleaned code

--- feury_partner_tags/wizard/mail_compose_message.py
+++ feury_partner_tags/wizard/mail_compose_message.py
@@ -4,21 +4,33 @@
 from odoo import api, fields, models, _
 
 
+SUPPORTED_MODELS = ['sale.order', 'account.move', 'stock.picking']
+
+
 class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
     @api.model
     def generate_email_for_composer(self, template_id, res_ids, fields=None):
+        PARTNER_CATEGORY = self.env['res.partner.category']
+
         values = super(MailComposeMessage, self).generate_email_for_composer(template_id, res_ids, fields=None)
-        if self.env.context.get('default_model') and \
-            self.env.context['default_model'] in ['sale.order', 'account.move', 'stock.picking'] and \
-            self.env.context.get('change_partners'):
+        add_partners = self.env.context.get('change_partners', False)
+        active_model = self.env.context.get('default_model', False)
+        res_id = self.env.context['default_res_id']
+
+        if active_model and active_model in SUPPORTED_MODELS and add_partners:
             for res_id in res_ids:
-                record = self.env[self.env.context['default_model']].browse(self.env.context['default_res_id'])
-                partner_categories = self.env['res.partner.category'].search(
-                    [('trigger_model_name', '=', self.env.context['default_model'])])
+                MODEL = self.env[active_model]
+                record = MODEL.browse(res_id)
+                partner_categories = PARTNER_CATEGORY.search(
+                    [
+                        ('trigger_model_name', '=', active_model)
+                    ]
+                )
                 if partner_categories and record.partner_id.child_ids:
                     partner_ids = record.partner_id.child_ids.filtered(
-                        lambda c: any([categ in partner_categories for categ in c.category_id]))
+                        lambda c: any(categ in partner_categories for categ in c.category_id)
+                    )
                     values[res_id]['partner_ids'] = partner_ids or values[res_id]['partner_ids']
         return values
