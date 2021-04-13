beb0b25c71be7e3a70853d6326a1cbac628f02cc
rachid-el-kedmiri <1028029592@usmba.ac.ma>
2021-04-13 10:47:32 +0000

    [ADD] Added module pricelist

---
+++ feury_pricelist/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Feury Pricelist",
+    'version': '1.0',
+    'depends': [
+        'feury_artwork',
+    ],
+    'author': 'Business Software Development Solutions',
+    'license': 'AGPL-3',
+    'mainainer': 'Business Software Development Solutions',
+    'category': 'sales',
+    'description': "Pricelist",
+    'data': [
+        # 'security/groups.xml',
+        # 'security/ir.model.access.csv',
+        # 'security/ir_rule.xml',
+        'views/res_partner.xml',
+        # 'views/embellishment.xml',
+        # 'views/clothes_location.xml',
+        # 'views/clothes_type.xml',
+        # 'views/menu.xml',
+        # 'data/clothes_location.xml',
+        # 'data/clothes_type.xml',
+    ],
+}
