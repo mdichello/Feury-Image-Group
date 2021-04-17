308d4c4bea59c5daaf4c1b69974a84aaf610d532
rachid-el-kedmiri <1028029592@usmba.ac.ma>
2021-04-17 01:51:11 +0000

    [ADD] add constraint to not allow lines with zero cost.

--- feury_pricelist/models/customer_pricelist.py
+++ feury_pricelist/models/customer_pricelist.py
@@ -11,7 +11,6 @@ from collections import defaultdict
 
 
 # TODO add a thumbnail (search if we can have a selection from available ones)
-# TODO add constraint to not allow lines with zero cost.
 # TODO add button called "refresh", actions: update the cost of each group.
 # TODO restrict extra types on the line to (heat_seal, sew_patch, embroider).
 # TODO check duplicated values in lines!
@@ -153,18 +152,20 @@ class CustomerPricelist(models.Model):
     # 2- Constraints methods (_check_***)
     # ----------------------------------------------------------------------------------------------------
 
-    @api.constrains('line_ids')
+    @api.constrains('state')
     def _check_lines(self):
         for record in self:
-            # We should have at least one line
-            if not record.line_ids:
+            # We should have at least one line.
+            if record.state != 'draft' and not record.line_ids:
                 raise ValidationError(_('You must have at least one item in the pricelist.'))
 
             # Check if we have a zero cost line.
             if record.state in ('sent', 'signed', 'approved'):
                 costs = record.line_ids.mapped('cost')
                 if any(cost <= 0 for cost in costs):
-                    raise ValidationError(_('You can not have a zero cost line in the state.'))
+                    raise ValidationError(_(
+                        'Sorry: You can not have a pricelist item with cost zero in the state %s.'
+                    ) % (record.state, ))
 
     # ----------------------------------------------------------------------------------------------------
     # 3- Compute methods (namely _compute_***)
